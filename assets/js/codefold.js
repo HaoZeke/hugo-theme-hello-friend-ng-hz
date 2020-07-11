83b928cc7b1bebdd15d5b2e9e809e9121a4c5b17
Rohit Goswami <rgoswami@ieee.org>
2020-07-11 21:29:08 +0000

    codefold: Init new feature

---
+++ assets/js/codefold.js
@@ -0,0 +1,8 @@
+new Readmore("pre", {
+  speed: 75,
+  heightMargin: 25,
+  lessLink:
+    '<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↑↑</a></div></div></div>',
+  moreLink:
+    '<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↓↓</a></div></div></div>',
+});
