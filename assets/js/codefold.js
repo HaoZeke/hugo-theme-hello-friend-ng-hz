96d2777dc937d9e2d60c7a91a20ef1d31e698ff2
Rohit Goswami <rgoswami@ieee.org>
2020-07-12 00:07:35 +0000

    codefold: Move back up on close

--- assets/js/codefold.js
+++ assets/js/codefold.js
@@ -5,4 +5,10 @@ new Readmore("pre", {
     '<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↑↑</a></div></div></div>',
   moreLink:
     '<div class="fold"><div class="button"><div class="button__text fold"><a href="#">↓↓</a></div></div></div>',
+  afterToggle: function (trigger, element, expanded) {
+    if (!expanded) {
+      // The "Close" link was clicked
+      window.scrollTo({ top: element.offsetTop, behavior: "smooth" });
+    }
+  },
 });
