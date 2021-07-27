3dd59881fc48898b64ca7cb8a3ce62efb835531f
Rohit Goswami <rog32@hi.is>
2021-07-27 15:54:36 +0000

    nix: Pin to pandoc by pinning to haskellpackages

--- shell.nix
+++ shell.nix
@@ -2,13 +2,17 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
   stdenv = pkgs.stdenv;
-  myHaskellEnv = pkgs.haskellPackages.ghcWithPackages
-    (haskellPackages: with haskellPackages; [ pandoc_2_10_1 pandoc-citeproc ]);
-  # Don't go beyond 2.10.1 until https://github.com/kaushalmodi/ox-hugo/issues/336 is closed
   nodePkgs = (pkgs.callPackage ./node.nix {
     inherit pkgs;
     nodejs = pkgs.nodejs-12_x;
   }).shell.nodeDependencies;
+  # haskellpkgs is a pinned version of nixpkgs
+  # https://github.com/HaoZeke/haozeke.github.io/commit/e312ff597931fde168e4da6aec37164ef2616894#diff-a0745f7ed88cfac6058d1d4cfb57bc71127a28ad7c6756004142a90aab5ee99f
+  hpkgs = import sources.haskellpkgs {};
+  myHaskellEnv = hpkgs.haskellPackages.ghcWithPackages
+    (haskellPackages: with haskellPackages; [ pandoc_2_10_1 pandoc-citeproc ]);
+  # Don't go beyond 2.10.1 until https://github.com/kaushalmodi/ox-hugo/issues/336 is closed
+  # https://github.com/vaibhavsagar/website/blob/master/blog/quick-easy-nixpkgs-pinning.md
   hook = ''
     eval $(egrep ^export ${myHaskellEnv}/bin/ghc)
     ln -s ${nodePkgs}/lib/node_modules ./node_modules
